b0517c870d0b5484f8521a80d15b8016d6bae58e
Eric Blanvillain <b00684319@essec.edu>
2022-02-11 17:47:10 +0100

    updated dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,3 +1,5 @@
+# made by EB
+
 import streamlit as st
 import streamlit.components.v1 as components
 import os
@@ -13,10 +15,10 @@ import plotly
 import shap
 
 
-# Title 
-st.markdown("<h1 style='text-align: center; color: #E2383F;'><strong>📈 PRET A DEPENSER DASHBOARD</u></strong></h1>", unsafe_allow_html=True)
+# Title
+st.markdown("<h1 style='text-align: center; color: #E2383F;'><strong>📈 PRET A DEPENSER</u></strong></h1>", unsafe_allow_html=True)
 # Subtitle
-st.markdown("<h4 style='text-align: center'><i>“A timely return of the loan makes it easier to borrow a second time.”</i></h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center'><i>“interactif dashboard”</i></h4>", unsafe_allow_html=True)
 st.markdown("***")
 
 DATA_FILE = 'data_final.csv'
@@ -24,19 +26,19 @@ DATA_FILE = 'data_final.csv'
 # Load
 @st.cache(persist = True)
 def load_all():
-    
+
     def load_data():
         data = pd.read_csv(DATA_FILE)
-        
-        data_shap = data.drop(columns=['Prediction', 'Prediction_Score', 
-        'PREDICTION_NEIGHBORS_20_MEAN','GENDER', 'NAME_CONTRACT_TYPE', 
+
+        data_shap = data.drop(columns=['Prediction', 'Prediction_Score',
+        'PREDICTION_NEIGHBORS_20_MEAN','GENDER', 'NAME_CONTRACT_TYPE',
         'NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS',
         'NAME_HOUSING_TYPE','YEARS_BIRTH','YEARS_EMPLOYED'])
         #data_shap = data_shap.set_index('SK_ID_CURR')
         return data, data_shap
     # load data
     data, data_shap = load_data()
-    
+
     def load_model():
         # load
         model = load('best_model.joblib')
@@ -102,7 +104,7 @@ resultats["Prediction_name"]= resultats["Prediction"].replace({0 : 'Non defaulte
 client_cible = resultats[resultats["SK_ID_CURR"]==client_id].iloc[0]["Prediction_name"]
 st.info("**Prediction of the selected client with the current threshold : ** : **{}** ".format(client_cible))
 
-# Obtenir le score de prédiction (moyenne) des 20 plus proches profiles clients 
+# Obtenir le score de prédiction (moyenne) des 20 plus proches profiles clients
 similar_clients_credit_score = data[data["SK_ID_CURR"]==client_id].iloc[0]["PREDICTION_NEIGHBORS_20_MEAN"]
 
 #https://plotly.com/python/gauge-charts/
@@ -111,7 +113,7 @@ gauge = go.Figure(go.Indicator(
         title = {'text': "Prediction Score", 'font': {'size': 24}},
         value = client_score,
         mode = "gauge+delta+number",
-        
+
         gauge = {'axis': {'range': [None, 100]},
                  'steps' : [
                      {'range': [0, 20], 'color': "#008177"},
@@ -133,7 +135,7 @@ gauge = go.Figure(go.Indicator(
         'decreasing' : {'color' : '#008177'}}
         ))
 
-gauge.update_layout(width=600, height=500, 
+gauge.update_layout(width=600, height=500,
                     margin=dict(l=50, r=50, b=100, t=100, pad=4))
 
 st.plotly_chart(gauge)
@@ -158,10 +160,10 @@ left_column_1.markdown('Prediction Score for similar clients : **{0:.1f}**'.form
 ###############      SIDEBAR        #######################
 
 # Chargement du logo
-logo =  Image.open("Images/pret_logo.png") 
+logo =  Image.open("Images/pret_logo.png")
 st.sidebar.image(logo,
                 width=240,
-                caption="Now you can")
+                caption="by EB")
 
 # Préparation informations client sélectionné
 
@@ -184,7 +186,7 @@ st.sidebar.write("**Education** :", client_education)
 st.sidebar.write("**Years employed**", int(client_employed), "years")
 st.sidebar.write("**Income type** :", client_work)
 st.sidebar.write("**Income**", int(client_income), "$")
-st.sidebar.write("**Contract type** :", client_contract) 
+st.sidebar.write("**Contract type** :", client_contract)
 
 
 # Préparation informations clientèle totale
@@ -229,7 +231,7 @@ if st.sidebar.button("Age"):
                 'thickness': 0.75,
                 'value': client_age
             },
-                'steps': [{'range': [client_age_min, client_age_mean], 'color': "#0093B2"}, 
+                'steps': [{'range': [client_age_min, client_age_mean], 'color': "#0093B2"},
                 {'range': [client_age_mean, client_age_max], 'color': "#B8DDE1"}],
                 'bar': {'color': "#002E5D"}
             }))
@@ -244,13 +246,13 @@ if st.sidebar.button("Gender"):
         left_column_1.header("**Stats : gender**")
         left_column_1.success("**Gender of the selected client** : **{}** ".format(client_gender))
         st.subheader('Distribution of code gender for all targets')
-        img_status =  Image.open("Images/code_gender_1.png") 
+        img_status =  Image.open("Images/code_gender_1.png")
         st.image(img_status,
                     width=400)
-        st.subheader('Percentage of **defaulters** for each category of code gender')            
-        img_status =  Image.open("Images/code_gender_2.png") 
+        st.subheader('Percentage of **defaulters** for each category of code gender')
+        img_status =  Image.open("Images/code_gender_2.png")
         st.image(img_status,
-                    width=400)  
+                    width=400)
 
 # Family status
 if st.sidebar.button("Family status"):
@@ -260,13 +262,13 @@ if st.sidebar.button("Family status"):
         left_column_1.header("**Stats : family status**")
         left_column_1.success("**Family status of the selected client** : **{}** ".format(client_status))
         st.subheader('Distribution of family status')
-        img_status =  Image.open("Images/status_1.png") 
+        img_status =  Image.open("Images/status_1.png")
         st.image(img_status,
                     width=600)
-        st.subheader('Percentage of **defaulters** for each category of family status')            
-        img_status =  Image.open("Images/status_2.png") 
+        st.subheader('Percentage of **defaulters** for each category of family status')
+        img_status =  Image.open("Images/status_2.png")
         st.image(img_status,
-                    width=600)           
+                    width=600)
 # Education
 if st.sidebar.button("Education "):
     st.sidebar.write("**Education** :", client_education)
@@ -275,13 +277,13 @@ if st.sidebar.button("Education "):
         left_column_1.header("**Stats : Education type**")
         left_column_2.success("**Education type of the selected client** : **{}** ".format(client_education))
         st.subheader('Distribution of education type')
-        img_status =  Image.open("Images/education_1.png") 
+        img_status =  Image.open("Images/education_1.png")
         st.image(img_status,
                     width=600)
-        st.subheader('Percentage of **defaulters** for each category of education')            
-        img_status =  Image.open("Images/education_2.png") 
+        st.subheader('Percentage of **defaulters** for each category of education')
+        img_status =  Image.open("Images/education_2.png")
         st.image(img_status,
-                    width=600)      
+                    width=600)
 
 # Emploi : durée
 if st.sidebar.button("Years employed"):
@@ -309,12 +311,12 @@ if st.sidebar.button("Years employed"):
                 'thickness': 0.75,
                 'value': client_employed
             },
-            'steps': [{'range': [client_employed_min, client_employed_mean], 'color': "#0093B2"}, 
+            'steps': [{'range': [client_employed_min, client_employed_mean], 'color': "#0093B2"},
                 {'range': [client_employed_mean, client_employed_max], 'color': "#B8DDE1"}],
                 'bar': {'color': "#002E5D"}
             }))
         fig.update_layout(width=700, height=300, margin=dict(l=180, r=50, b=100, t=100, pad=4))
-        st.plotly_chart(fig)    
+        st.plotly_chart(fig)
 
 # Revenus du client
 if st.sidebar.button("Income type"):
@@ -324,18 +326,18 @@ if st.sidebar.button("Income type"):
         left_column_1.header("**Stats : income type**")
         left_column_1.success("**Income type of the selected client** : **{}** ".format(client_work))
         st.subheader('Distribution of income type')
-        img_status =  Image.open("Images/income_type_1.png") 
+        img_status =  Image.open("Images/income_type_1.png")
         st.image(img_status,
                     width=400)
         st.subheader('Percentage of **defaulters** for each category of income type')
-        img_status =  Image.open("Images/income_type_2.png") 
+        img_status =  Image.open("Images/income_type_2.png")
         st.image(img_status,
                     width=400)
 
 if st.sidebar.button("Income"):
     st.sidebar.write("**Income**", int(client_income), "$")
      # Graph dans app principale
-    if st.sidebar.checkbox("Show income infos / Hide", value = True): 
+    if st.sidebar.checkbox("Show income infos / Hide", value = True):
         left_column_1.header("**Client : income**")
         left_column_1.success("**Income of the selected client** : **{}** ".format(int(client_income)))
         fig = go.Figure(go.Indicator(
@@ -357,25 +359,25 @@ if st.sidebar.button("Income"):
                 'thickness': 0.75,
                 'value': client_income
             },
-                'steps': [{'range': [client_income_min, client_income_mean], 'color': "#0093B2"}, 
+                'steps': [{'range': [client_income_min, client_income_mean], 'color': "#0093B2"},
                 {'range': [client_income_mean, client_income_max], 'color': "#B8DDE1"}],
                 'bar': {'color': "#002E5D"}
             }))
         fig.update_layout(width=700, height=300, margin=dict(l=110, r=50, b=100, t=100, pad=4))
-        st.plotly_chart(fig) 
+        st.plotly_chart(fig)
 
 if st.sidebar.button("Contract type"):
-    st.sidebar.write("**Contract type** :", client_contract)      
+    st.sidebar.write("**Contract type** :", client_contract)
     # Graph dans app principale
     if st.sidebar.checkbox("Show type of loans stats / Hide", value = True):
         left_column_1.header("**Stats : contract type**")
         left_column_1.success("**Type of loan of the selected client** : **{}** ".format(client_contract))
         st.subheader('Distribution of contract type')
-        img_status =  Image.open("Images/contract_1.png") 
+        img_status =  Image.open("Images/contract_1.png")
         st.image(img_status,
                     width=400)
         st.subheader('Percentage of **defaulters** of contract type for all targets')
-        img_status =  Image.open("Images/contract_2.png") 
+        img_status =  Image.open("Images/contract_2.png")
         st.image(img_status,
                     width=400)
 
@@ -392,10 +394,10 @@ def shap_explainer():
             st.write('*__Force plot__ shows, how opposite are the features strenghs*')
             if st.sidebar.checkbox("SHAP / Hide", value = True):
                 st.write('**SHAP HELP**')
-                img_help_shap =  Image.open("Images/shap_explain.png") 
+                img_help_shap =  Image.open("Images/shap_explain.png")
                 st.image(img_help_shap,
                         width=700)
-    
+
             explainer = shap.TreeExplainer(model)
             row_to_show = data_shap[data_shap["SK_ID_CURR"]==client_id].index
             data_cal = data_shap.set_index("SK_ID_CURR")
@@ -403,9 +405,9 @@ def shap_explainer():
             data_for_prediction_array = data_for_prediction.values.reshape(1, -1)
             # Calculate Shap values
             shap_values = explainer.shap_values(data_for_prediction_array)
-            ind_fig = shap.force_plot(explainer.expected_value[1], 
-                            shap_values[1], 
-                            data_for_prediction, 
+            ind_fig = shap.force_plot(explainer.expected_value[1],
+                            shap_values[1],
+                            data_for_prediction,
                             plot_cmap=["#EF553B","#636EFA"])
             ind_fig_html = f"<head>{shap.getjs()}</head><body>{ind_fig.html()}</body>"
             st.write('**SHAP Force plot for the selected client**')
